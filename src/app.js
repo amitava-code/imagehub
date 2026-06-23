f602387f0a524e45f2ac00d3c54a1164c3c296d7
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-23 20:23:57 +0530

    feat(post): store uploaded image URL and caption in database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer')
 const uploadFile = require("./services/storage.service")
+const postModel = require("./models/post.model")
+
 
 const app = express();
 app.use(express.json());
@@ -11,12 +13,21 @@ const upload = multer({storage: multer.memoryStorage()})
 
 app.post('/create-post', upload.single("image") ,async(req, res)=>{
 
-    console.log(req.body);
-    console.log(req.file)
-
     const result = await uploadFile(req.file.buffer)
 
-    console.log(result)
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+    return res.status(201).json({
+        message: "Post created Successfully",
+        post
+    })
+
+
+
+    
 })
 
 module.exports= app;
